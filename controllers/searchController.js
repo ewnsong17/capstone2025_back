7e0be4c18c618018993d4b03238721c5413d1702
ewnsong17 <ewnsong17@naver.com>
2025-04-12 20:26:42 +0900

    session 추가

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -5,8 +5,8 @@ class SearchController {
   // 컨트롤러 처리
   async getFilters(req, res) {
     try {
-  //    const users = await userService.getUsers(); // 서비스에서 데이터를 가져옴
-      res.status(200).json({result: true, filter_list: ['콘서트', '뮤지컬']});
+      const filter_list = await searchService.getFilterList(); // 서비스에서 데이터를 가져옴
+      res.status(200).json({result: true, filter_list: filter_list});
     } catch (error) {
       res.status(500).json({result: false, exception: error.message});
     }
