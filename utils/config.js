9de18139472e05a8edb5d2126f815569bc14ca42
narin-Oh <narin5654@naver.com>
2025-05-20 15:52:59 +0900

    이미지

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,13 @@
 // config.js
 const config = {
     server: {
+      ip: '223.194.155.13',
 			port: 3000,
     },
     db: {
       host: 'localhost',     // MySQL 서버의 호스트 주소
       user: 'root',          // MySQL 사용자명
-      password: 'csedbadmin',  // MySQL 비밀번호
+      password: 'root',  // MySQL 비밀번호
       database: 'capstone',   // 사용할 데이터베이스명
       port: 3306             // MySQL 포트 (기본값 3306)
     },
